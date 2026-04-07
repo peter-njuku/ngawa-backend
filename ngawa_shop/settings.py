53753d689d5bc699a20d4764169ab485c9b69746
peter-njuku <njuku2005@gmail.com>
2026-04-07 17:17:56 +0300

    CORS

--- ngawa_shop/settings.py
+++ ngawa_shop/settings.py
@@ -113,6 +113,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8000',
     'https://localhost:3000',
+    'https://v0-ngawa-solutions-website-ry.vercel.app/',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
