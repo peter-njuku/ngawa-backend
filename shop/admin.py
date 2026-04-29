a19cc54dfe98e2da9ae5ba51b7aa3a5c2c4c4682
peter-njuku <njuku2005@gmail.com>
2026-04-29 11:35:41 +0300

    Images - cloudinary

--- shop/admin.py
+++ shop/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import Category, Product
 
-@admin.site.register(Category)
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ('name',)
 
-@admin.site.register(Product)
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'in_stock', 'featured', 'created_at')
     list_filter = ('category', 'in_stock', 'featured', 'created_at')
