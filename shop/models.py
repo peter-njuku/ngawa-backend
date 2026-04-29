4004cb0272c93b0903e6f878a9a157c643dc99b8
peter-njuku <njuku2005@gmail.com>
2026-04-29 11:55:43 +0300

    Images - cloudinary

--- shop/models.py
+++ shop/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     image = CloudinaryField('images')
-    stock_quantity = models.IntegerField(default=0)
+    #stock_quantity = models.IntegerField(default=0)
     in_stock = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
