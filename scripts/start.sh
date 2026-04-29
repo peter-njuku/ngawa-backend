346582dc0d99447888ff6988e013970fdbcf1ac4
peter-njuku <njuku2005@gmail.com>
2026-04-29 11:41:05 +0300

    Images - cloudinary

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "Collecting static files..."
-python manage.py collectstatic 
+python manage.py collectstatic --noinput 
 
 echo "Applying database migrations..."
 python manage.py makemigrations --noinput
